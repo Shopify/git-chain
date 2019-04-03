fb06138d5c0bb643f5054f0692f77d59355c7671
Robin Brandt <robin.brandt@shopify.com>
2019-04-03 14:51:24 -0400

    Add fixture to generate a repo with chains set up

---
+++ fixtures/a-b-c-chain.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+git init >/dev/null
+
+for b in master a b c; do
+    git checkout -b $b 2>/dev/null
+    git commit --allow-empty -m "$b.1" >/dev/null
+done
+
+for b in master a b c; do
+    git checkout $b 2>/dev/null
+    git commit --allow-empty -m "$b.2" >/dev/null
+done
+
+PREV=""
+for b in master a b c; do
+    git config branch.$b.chain default
+    if [ ! -z "$PREV" ]; then
+        git config branch.$b.parentBranch $PREV
+        git config branch.$b.branchPoint $(git rev-parse $PREV)
+    fi
+    PREV=$b
+done
