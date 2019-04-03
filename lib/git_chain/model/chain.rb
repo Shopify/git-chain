d00e016debe0205c9bde0efed8d1f415068832e9
Robin Brandt <robin.brandt@shopify.com>
2019-04-03 16:25:03 -0400

    Sort branches

--- lib/git_chain/model/chain.rb
+++ lib/git_chain/model/chain.rb
@@ -9,61 +9,29 @@ module GitChain
       end
 
       class << self
-        def all
-          branch_by_name = {}
-
-          branch_config_lines = <<~EOS
-            branch.master.chain default
-            branch.a.chain default
-            branch.a.parentbranch master
-            branch.a.branchpoint 8fd7c8bf2f643d47785f4726ca39bdb913eb576b
-            branch.b.chain default
-            branch.b.parentbranch a
-            branch.b.branchpoint 3a3e22180a111603a693ba38716370c38ee1f8a1
-            branch.c.chain default
-            branch.c.parentbranch b
-            branch.c.branchpoint 71ce39ca6742084319b3362f64a26a6e1cbf639a
-          EOS
-
-          branch_config_lines.split("\n").each do |line|
-            key, value = line.split(" ")
-            key = key["branch.".length..-1]
-
-            key_parts = key.split(".")
-            branch_name = key_parts.first
-            attribute = key_parts[1]
-
-            branch_by_name[branch_name] ||= {}
-            branch_by_name[branch_name][attribute] = value
-          end
-
-          branch_by_name.map { |name, attributes| Branch.new(name: name, parent: attributes["parentbranch"], point: attributes["branchpoint"]) }
-        end
-
-        def sort(branches)
-          sorted = []
-          remaining = branches.clone
-
-          current_parent = nil
-          while !remaining.empty?
-            node = remaining.find { |branch| branch.parent == current_parent }
-            puts "node: #{node}"
-            sorted << node
-            remaining.delete(node)
-            current_parent = node
-          end
-
-          sorted
-        end
-
         def from_config(name)
           chains = Git.chains(chain_name: name)
           branches = chains.keys.map(&Branch.method(:from_config))
 
           new(
             name: name,
-            branches: branches,
+            branches: sort_branches(branches),
           )
+        end
+
+        def sort_branches(branches)
+          sorted = []
+          remaining = branches.clone
+
+          current_parent = nil
+          until remaining.empty?
+            node = remaining.find { |branch| branch.parent_branch == current_parent }
+            sorted << node
+            remaining.delete(node)
+            current_parent = node.name
+          end
+
+          sorted
         end
       end
     end
--- test/model/test_chain.rb
+++ test/model/test_chain.rb
@@ -9,10 +9,22 @@ module GitChain
         with_test_repository('a-b-c-chain') do
           chain = Chain.from_config('default')
           assert_equal('default', chain.name)
-          # TODO, change to ordered
-          require 'set'
-          assert_equal(Set.new(%w(master a b c)), Set.new(chain.branches.map(&:name)))
+          assert_equal(%w(master a b c), chain.branches.map(&:name))
         end
+      end
+
+      def test_sort
+        branches = {
+          master: Branch.new(name: "master", parent_branch: nil, branch_point: nil),
+          a: Branch.new(name: "a", parent_branch: "master", branch_point: nil),
+          b: Branch.new(name: "b", parent_branch: "a", branch_point: nil),
+          c: Branch.new(name: "c", parent_branch: "b", branch_point: nil),
+        }
+
+        unsorted = [branches[:master], branches[:a], branches[:c], branches[:b]]
+        expected = [branches[:master], branches[:a], branches[:b], branches[:c]]
+
+        assert_equal expected, Chain.sort_branches(unsorted)
       end
     end
   end
