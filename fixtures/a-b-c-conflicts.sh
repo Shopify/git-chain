941d64f2dbda150535557b3125f0359642626d75
Robin Brandt <robin.brandt@shopify.com>
2019-04-04 10:52:12 -0400

    Setup chain in conflict fixture

--- fixtures/a-b-c-conflicts.sh
+++ fixtures/a-b-c-conflicts.sh
@@ -4,11 +4,19 @@ set -e
 
 git init >/dev/null
 
+PREV=
 for b in master a b c; do
     git checkout -b $b 2>/dev/null
     echo Hello from $b > $b.txt
     git add $b.txt
     git commit -m "$b.1" >/dev/null
+
+    git config "branch.$b.chain" default
+    if [ ! -z "$PREV" ]; then
+        git config "branch.$b.branchParent" $PREV
+    fi
+
+    PREV=$b
 done
 
 git checkout a 2>/dev/null
@@ -18,9 +26,13 @@ git commit -m "a.2 (conflicting)" > /dev/null
 
 git checkout master >/dev/null
 
-* 7748320 (a) a.2 (conflicting)
-| * d522f2f (c) c.1
-| * 74a490d (b) b.1
-|/
-* d89f9f4 a.1
-* cc71e35 (HEAD -> master) master.1
+git config "branch.a.branchPoint" $(git rev-parse master)
+git config "branch.b.branchPoint" $(git rev-parse a^)
+git config "branch.c.branchPoint" $(git rev-parse b)
+
+# * 7748320 (a) a.2 (conflicting)
+# | * d522f2f (c) c.1
+# | * 74a490d (b) b.1
+# |/
+# * d89f9f4 a.1
+# * cc71e35 (HEAD -> master) master.1
