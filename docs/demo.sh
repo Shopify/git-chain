0c70f02a83f0803a881500a6f9388ceea8be3c7c
Sebastien Lavoie <sebastien.lavoie@shopify.com>
2019-04-05 13:43:53 -0400

    demo

--- docs/demo.sh
+++ docs/demo.sh
@@ -2,33 +2,35 @@
 
 TYPE_DELAY=0.06
 EOL_DELAY=1
-PROMPT=$
+PROMPT="\x1B[0;32m\$\x1B[0m"
 
 type() {
   args=$1
   local delay=$TYPE_DELAY
-  if [ "$2" == "fast" ]
+  local eol_delay=$EOL_DELAY
+  if [[ "$2" == "fast" ]]
   then
     delay=0.02
+    eol_delay=0.5
   fi
 
-  echo -n $PROMPT" "
+  echo -en "${PROMPT} "
 
   for (( i=0; i<${#args}; i++ ))
   do
     echo -n "${args:$i:1}"
     sleep $delay
   done
-  
-  sleep $EOL_DELAY
+
+  sleep $eol_delay
   echo
-  
+
   eval "$1"
   echo
 }
 
-show() {
-  echo "$1"
+comment() {
+  echo -e "\x1B[94m𝒾\x1B[0;36m$1\x1B[0m"
   sleep $EOL_DELAY
 }
 
@@ -52,51 +54,54 @@ clear
 #EOL_DELAY=0
 #TYPE_DELAY=0
 
-type "git log --oneline --graph --all"
-sleep 2
+#type "git log --oneline --graph --all"
+#sleep 2
 
+comment "Starting a chain for our \x1B[0;33mAwesome Feature\x1B[0;36m"
 type "git chain branch -c awesome-feature master add-database-table"
-show "## ... adding a table ..."
+
+comment "Adding a table"
 type "echo Some change > whatever.txt" fast
 type "git add ." fast
 type "git commit -m 'Add table'" fast
 
+comment "Add a \x1B[0;33msecond\x1B[0;36m branch to the chain"
 type "git chain branch add-model"
-show "## ... adding a model ..."
 type "echo Another change >> whatever.txt" fast
-type "git add ." fast
-type "git commit -m 'Add model'" fast
+type "git commit -a -m 'Add model'" fast
 
+comment "Add a \x1B[0;33mthird\x1B[0;36m branch to the chain"
 type "git chain branch add-ui"
-show "## ... adding a ui ..."
 type "echo Another change >> anotherfile.txt" fast
 type "git add ." fast
 type "git commit -m 'Add ui'" fast
- 
-show "## We now have a chain"
+
+comment "Our chain now has \x1B[0;33m3\x1B[0;36m branches"
 type "git chain list"
 
 sleep 2
 
+comment "Let's go back and add a commit to our \x1B[0;33mfirst\x1B[0;36m branch."
 type "git checkout add-database-table"
 type "echo More attributes >> whatever.txt" fast
-type "git add ." fast
-type "git commit -m 'Modified table'" fast
+type "git commit -a -m 'Modified table'" fast
 
+comment "And add a commit to our \x1B[0;33msecond\x1B[0;36m branch for good measure."
 type "git checkout add-model"
 type "echo More attributes >> model_only.txt" fast
 type "git add ." fast
 type "git commit -m 'Modified model'" fast
 
-show "## now let's use git chain 🎉"
+comment "Now let's use \x1B[0;33mgit chain\x1B[0;36m and realign all of our branches."
 type "git chain rebase"
 
 #EOL_DELAY=1
 
-show "## a conflict 🙀 - let's resolve this 😁"
+comment "A conflict 🙀 - let's resolve this 😁"
 type "echo My changes are better > whatever.txt" fast
-type "git add ." fast
-type "git commit -m 'Add model (with changes)'"
+type "git commit -a -m 'Add model (with changes)'"
 type "git rebase --continue"
+
+comment "Now that the conflict is resolved, \x1B[0;33mgit chain\x1B[0;36m succeeds 🎉"
 type "git chain rebase"
 
