a501d8abd48a46b7c553ffbf97694418e44524d7
Sebastien Lavoie <sebastien.lavoie@shopify.com>
2021-03-24 15:10:27 -0400

    Add Github.parse_url

---
+++ test/git_chain/util/github_test.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+require "test_helper"
+
+module GitChain
+  module Util
+    class GithubTest < MiniTest::Test
+      def test_parse_url
+        %w(
+          git@github.com:Shopify/git-chain.git
+          ssh://git@github.com:Shopify/git-chain.git
+          https://github.com/Shopify/git-chain.git
+          http://github.com/Shopify/git-chain.git
+        ).each do |input|
+          url, org, repo = Github.parse_url(input)
+          assert_equal("https://github.com/Shopify/git-chain", url)
+          assert_equal("Shopify", org)
+          assert_equal("git-chain", repo)
+        end
+      end
+    end
+  end
+end
